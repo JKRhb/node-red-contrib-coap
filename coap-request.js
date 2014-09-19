340827bf74e3b695eabb0fa920bb6f14ef9beaed
Eelco Wiersma <eelco@pagebakers.nl>
2014-09-19 14:30:49 +0200

    Added CBOR support

--- coap-request.js
+++ coap-request.js
@@ -2,42 +2,85 @@ module.exports = function(RED) {
     "use strict";
     
     var coap = require('coap');
+    var cbor = require('cbor');
     var url = require('url');
+    
+    coap.registerFormat('application/cbor', 60);
   
     function CoapRequestNode(n) {
         RED.nodes.createNode(this, n);
         var node = this;
-
-        this.options = url.parse(n.url);
-        this.options.method = (n.method || 'GET').toUpperCase();
+        var method = (n.method || 'GET').toUpperCase();
 
         this.on('input', function(msg) {
-            if (msg.observe === true) {
-                node.options.observe = '1';
+
+            var opts = url.parse(n.url);
+        
+            opts.method = method;
+            opts.headers = {};
+        
+            if (n['content-format']) {
+                opts.headers['Content-Format'] = n['content-format'];
+            }
+            
+            var payload = null;
+
+            if (typeof msg.payload === "string" || Buffer.isBuffer(msg.payload)) {
+                payload = msg.payload;
+            } else if (typeof msg.payload == "number") {
+                payload = msg.payload+"";
             } else {
-                delete node.options.observe;
+                if (opts.headers['Content-Format'] === 'application/json') {
+                    payload = JSON.stringify(msg.payload);
+                } else if (opts.headers['Content-Format'] === 'application/cbor') {
+                    payload = cbor.encode(msg.payload);
+                }
+            }
+
+            if (msg.observe === true) {
+                opts.observe = '1';
+            } else {
+                delete opts.observe;
             }
 
             if (node.stream) {
                 node.stream.close();
             }
-
-            var req = coap.request(node.options);
+            
+            var req = coap.request(opts);
+            
             req.on('response', function(res) {
                 res.on('data', function(data) {
-                    var msg = {
-                        payload: JSON.parse(data.toString())
-                    };
-                    node.send(msg);
-                    node.status({});
+                    if (res.headers['Content-Format'] === 'application/json') {
+                        node.send({
+                            payload: JSON.parse(data.toString())
+                        });
+                    } else if (res.headers['Content-Format'] === 'application/cbor') {
+                        cbor.decode(data, function(err, data) {
+                            if (err) {
+                                console.error(err.message);
+                                return false;
+                            }
+                            node.send({
+                                payload: data
+                            });
+                        });
+                    } else {
+                        node.send({
+                            payload: data
+                        });
+                    }
                 });
 
-                if (node.options.observe) {
+                if (opts.observe) {
                     node.stream = res;
                 }
             });
+            if (payload) {
+                req.write(payload);
+            }
             req.end();
         });
     }
     RED.nodes.registerType("coap request", CoapRequestNode);
-}
+};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 /* used for testing, remove this file later */
 const coap    = require('coap') // or coap
+    , cbor    = require('cbor')
     , server  = coap.createServer();
+
+coap.registerFormat('application/cbor', 60);
 
 function getPayload() {
     var timestamp = Math.round(new Date().getTime() / 1000);
@@ -14,18 +17,34 @@ function getPayload() {
             temperature: timestamp
         }
     };
-    return JSON.stringify(payload);
+    return payload;
 }
 
 server.on('request', function(req, res) {
-    res.setOption('Content-Format', 'application/json')
     
+    console.log(req.payload);
+    
+    var encode;
+    
+    if (req.headers['Content-Format'] === 'application/json') {
+        res.setOption('Content-Format', 'application/json');
+        encode = JSON.stringify;
+        payload = JSON.parse(req.payload);
+        console.log(payload);
+        
+    } else if(req.headers['Content-Format'] === 'application/cbor') {
+        res.setOption('Content-Format', 'application/cbor');
+        encode = cbor.encode;
+        cbor.decode(req.payload, function(err, payload) {
+            console.log(payload[0]);
+        });
+    }
 
     if (req.headers['Observe'] !== 0)
-      return res.end(getPayload());
+      return res.end(encode(getPayload()));
     
     var interval = setInterval(function() {
-      res.write(getPayload());
+      res.write(encode(getPayload()));
     }, 1000);
 
     res.on('finish', function(err) {
