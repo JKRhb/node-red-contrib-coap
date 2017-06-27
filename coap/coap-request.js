0aa52ef62973a705c50dbb74d143997e335b5ead
BeWiBu <bernd.wissler@kbr.de>
2017-06-27 12:05:12 +0200

    Update coap-request.js

--- coap/coap-request.js
+++ coap/coap-request.js
@@ -8,15 +8,22 @@ module.exports = function(RED) {
 
     coap.registerFormat('application/cbor', 60);
 
+	var states={
+		none:0,
+		requested:1,
+		responded:2,
+	};
     function CoapRequestNode(n) {
         RED.nodes.createNode(this, n);
         var node = this;
+		
+		var connectstate=states.none;
 
         // copy "coap request" configuration locally
         node.options = {};
         node.options.method = n.method||"GET";
         node.options.observe = n.observe||false;
-	node.options.observe_on_start = n.observe_on_start||false;
+		node.options.observe_on_start = n.observe_on_start||false;
         node.options.name = n.name;
         node.options.url = n.url;
         node.options.contentFormat = n['content-format'];
@@ -59,24 +66,30 @@ module.exports = function(RED) {
             reqOpts.method = ( node.options.method || msg.method || 'GET' ).toUpperCase();
             reqOpts.headers = {};
             reqOpts.headers['Content-Format'] = node.options.contentFormat;
+			reqOpts.timeout=10;
             function _onResponse(res) {
+				
                 function _onResponseData(data) {
+					node.status({fill:"green",shape:"dot",text:"Connected "+(reqOpts.observe ? "(observed)":"")+new Date()});
                     var payload = null;
                     if ( node.options.rawBuffer ) {
                         node.send({
                             payload: data,
+							state: 1
                         });
                         onPayloadDecoded(data);
                     } else if (res.headers['Content-Format'] === 'text/plain') {
                         payload = data.toString();
                         node.send({
                             payload: payload,
+							state: 1
                         });
                         onPayloadDecoded(payload);
                     } else if (res.headers['Content-Format'] === 'application/json') {
                         payload = JSON.parse(data.toString());
                         node.send({
                             payload: payload,
+							state: 1
                         });
                         onPayloadDecoded(payload);
                     } else if (res.headers['Content-Format'] === 'application/cbor') {
@@ -85,11 +98,13 @@ module.exports = function(RED) {
                         payload = linkFormat.parse( data.toString() );
                         node.send({
                             payload: payload,
+							state: 1
                         });
                         onPayloadDecoded(payload);
                     } else {
                         node.send({
                             payload: data.toString(),
+							state: 1
                         });
                         onPayloadDecoded(data.toString());
                     }
@@ -115,11 +130,25 @@ module.exports = function(RED) {
                 node.stream.close();
             }
 
+			node.status({fill:"yellow",shape:"ring",text:"Pending ("+(reqOpts.observe ? "Observed ":"")+reqOpts.method+" request)"});
+			
+			
             var req = coap.request(reqOpts);
+			
             req.on('response', _onResponse);
             req.on('error', function(err) {
                 node.log('client error');
                 node.log(err);
+				node.status({fill:"red",shape:"ring",text:"Error:"+err});
+				node.send({
+					state: 0
+				});
+            });
+			req.on('timeout', function() {
+				node.status({fill:"red",shape:"ring",text:"Timeout"});
+				node.send({
+					state: 0
+				});
             });
 
             if (payload) {
@@ -135,15 +164,17 @@ module.exports = function(RED) {
 		this.on('close', function(done) {
 			if (node.stream) {
 				node.stream.close();
-			}
+            }
 			done();
-		});
+        });
 		
+		node.status({});
 		if(node.options.observe && node.options.observe_on_start){
 			var dummyPayload={payload:""};
 			_makeRequest(dummyPayload);
 		}
     }
+	
 	
     RED.nodes.registerType("coap request", CoapRequestNode);
 };
