a4513edd71ff07f769e0ffda09bbe914bfe74123
Jan Romann <jan.romann@gmail.com>
2021-03-15 17:02:25 +0100

    feat(coap-in): Add coap reponse node

--- coap/coap-in.js
+++ coap/coap-in.js
@@ -1,6 +1,12 @@
 module.exports = function (RED) {
     "use strict";
-    var coap = require("coap");
+    const coap = require("coap");
+    const cbor = require("cbor");
+    const linkFormat = require("h5.linkformat");
+
+    function _checkContentFormat(contentFormat, expectedContentFormat) {
+        return typeof contentFormat === "string" && contentFormat.includes(expectedContentFormat);
+    }
 
     // A node red node that sets up a local coap server
     function CoapServerNode(config) {
@@ -10,7 +16,7 @@ module.exports = function (RED) {
         node._inputNodes = []; // collection of "coap in" nodes that represent coap resources
 
         // Setup node-coap server and start
-        var serverSettings = {};
+        const serverSettings = {};
         if (config.ipv6) {
             serverSettings.type = "udp6";
         } else {
@@ -37,8 +43,8 @@ module.exports = function (RED) {
 
     CoapServerNode.prototype.registerInputNode = function (inputNode) {
         var node = this;
-        var exists = false;
-        for (var i = 0; i < node._inputNodes.length; i++) {
+        let exists = false;
+        for (let i = 0; i < node._inputNodes.length; i++) {
             if (
                 node._inputNodes[i].options.url == inputNode.options.url &&
                 node._inputNodes[i].options.method == inputNode.options.method
@@ -56,18 +62,51 @@ module.exports = function (RED) {
         }
     };
 
+    function _getPayload(inNode, rawBuffer, payload, contentFormat) {
+        if (rawBuffer) {
+            return payload;
+        } else if (_checkContentFormat(contentFormat, "text/plain")) {
+            return payload.toString();
+        } else if (_checkContentFormat(contentFormat, "json")) {
+            return JSON.parse(payload.toString());
+        } else if (_checkContentFormat(contentFormat, "cbor")) {
+            cbor.decodeAll(payload, function (err, decodedPayload, req, res) {
+                if (err) {
+                    inNode.error(err);
+                }
+                return decodedPayload[0];
+            });
+        } else if (_checkContentFormat(contentFormat, "application/link-format")) {
+            return linkFormat.parse(payload.toString());
+        } else {
+            return payload.toString();
+        }
+    }
+
+    function _onRequest(inNode, req, res) {
+        const contentFormat = req.headers["Content-Format"];
+        const payload = _getPayload(inNode, inNode.options.rawBuffer, req.payload, contentFormat);
+
+        inNode.send({
+            payload,
+            contentFormat,
+            req,
+            res
+        });
+    }
+
     CoapServerNode.prototype.handleRequest = function (req, res) {
         //TODO: Check if there are any matching resource. If the resource is .well-known return the resource directory to the client
-        var node = this;
-        var matchResource = false;
-        var matchMethod = false;
-        for (var i = 0; i < node._inputNodes.length; i++) {
+        const node = this;
+        let matchResource = false;
+        let matchMethod = false;
+        for (let i = 0; i < node._inputNodes.length; i++) {
             if (node._inputNodes[i].options.url == req.url) {
                 matchResource = true;
                 if (node._inputNodes[i].options.method == req.method) {
                     matchMethod = true;
-                    var inNode = node._inputNodes[i];
-                    inNode.send({ req: req, res: res });
+                    const inNode = node._inputNodes[i];
+                    _onRequest(inNode, req, res);
                 }
             }
         }
@@ -92,13 +131,72 @@ module.exports = function (RED) {
         node.options.server = config.server;
         node.options.url = config.url.charAt(0) == "/" ? config.url : "/" + config.url;
 
-        var serverConfig = RED.nodes.getNode(config.server);
+        const serverConfig = RED.nodes.getNode(config.server);
 
-        if (serverConfig) {
+        if (serverConfig != null) {
             serverConfig.registerInputNode(node);
         } else {
             node.error("Missing server configuration");
         }
     }
     RED.nodes.registerType("coap in", CoapInNode);
+
+    function _constructPayload(msg, contentFormat) {
+        const payload = msg.payload;
+
+        if (payload == null) {
+            return null;
+        }
+
+        if (_checkContentFormat(contentFormat, "text/plain")) {
+            return msg.payload.toString();
+        } else if (_checkContentFormat(contentFormat, "json")) {
+            return JSON.stringify(msg.payload);
+        } else if (_checkContentFormat(contentFormat, "cbor")) {
+            return cbor.encode(msg.payload);
+        } else {
+            return msg.payload.toString();
+        }
+    }
+
+    function CoapOutNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.options = {
+            name: config.name,
+            code: config.code,
+            contentFormat: config.contentFormat
+        };
+
+        this.on("input", function (msg, _send, done) {
+            let code = this.options.code || msg.statusCode || "2.05";
+            // TODO: Improve contentFormat system
+            const contentFormat = msg.contentFormat || node.options.contentFormat;
+            const payload = _constructPayload(msg, contentFormat);
+
+            if (msg.res) {
+                msg.res.code = code;
+                if (contentFormat != null) {
+                    msg.res.setOption("Content-Format", contentFormat);
+                }
+
+                msg.res.on("error", function (err) {
+                    node.log("server error");
+                    node.log(err);
+                });
+
+                if (payload != null) {
+                    msg.res.write(payload);
+                }
+
+                msg.res.end();
+            } else {
+                node.error("No response found in input node!");
+            }
+
+            done();
+        });
+    }
+    RED.nodes.registerType("coap response", CoapOutNode);
 };
